d2cbe5de83272fb5361da8b4d5ed96653957165f
XuZhou <xzhou49@stevens.edu>
2023-09-22 10:41:45 -0400

    Fix bugs and retest

--- HW02a/TestTriangle.py
+++ HW02a/TestTriangle.py
@@ -20,15 +20,39 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleA(self):
         self.assertEqual(classifyTriangle(3, 4, 5),
-                         'InvalidInput', '3,4,5 is a Right triangle')
+                         'Right', '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self):
         self.assertEqual(classifyTriangle(5, 3, 4),
-                         'InvalidInput', '5,3,4 is a Right triangle')
+                         'Right', '5,3,4 is a Right triangle')
 
     def testEquilateralTriangles(self):
         self.assertEqual(classifyTriangle(1, 1, 1),
-                         'InvalidInput', '1,1,1 should be equilateral')
+                         'Equilateral', '1,1,1 should be equilateral')
+
+    def testNotATriangleA(self):
+        self.assertEqual(classifyTriangle(1, 2, 3),
+                         'NotATriangle', '1,2,3 is not a triangle')
+
+    def testNotATriangleB(self):
+        self.assertEqual(classifyTriangle('1', 2, 5),
+                         'NotATriangle', '"1",2,5 is not a triangle')
+
+    def testNotATriangleC(self):
+        self.assertEqual(classifyTriangle(1, 2, -1),
+                         'NotATriangle', '1,2,-1 is not a triangle')
+
+    def testIsocelesTriangleA(self):
+        self.assertEqual(classifyTriangle(1, 2, 2),
+                         'Isoceles', '1,2,2 is an isoceles triangle')
+
+    def testIsocelesTriangleB(self):
+        self.assertEqual(classifyTriangle(2, 1, 2),
+                         'Isoceles', '2,1,2 is an isoceles triangle')
+
+    def testScaleneTriangleA(self):
+        self.assertEqual(classifyTriangle(5, 6, 7),
+                         'Scalene', '5,6,7 is a scalene triangle')
 
 
 if __name__ == '__main__':
