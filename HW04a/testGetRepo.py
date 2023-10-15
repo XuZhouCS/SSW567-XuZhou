0f67f6c3f415672816e00293c829dc0048ddae74
XuZhou <xzhou49@stevens.edu>
2023-10-15 17:20:08 -0400

    fix travis failing

--- HW04a/testGetRepo.py
+++ HW04a/testGetRepo.py
@@ -1,17 +1,17 @@
 import unittest
-import getRepo
+from HW04a import getRepo
 from unittest.mock import patch
 
 
 class TestGithubAPI(unittest.TestCase):
-    @patch('getRepo.get_repo_commits')
+    @patch('HW04a.getRepo.get_repo_commits')
     def testAPIA(self, mock_get_repo_commits):
         mock_get_repo_commits.return_value = "Failed to fetch repositories for user XuZhou22222"
         result = getRepo.get_repo_commits('XuZhou22222')
         self.assertEqual(
             result, "Failed to fetch repositories for user XuZhou22222")
 
-    @patch('getRepo.get_repo_commits')
+    @patch('HW04a.getRepo.get_repo_commits')
     def testAPIB(self, mock_get_repo_commits):
         mock_get_repo_commits.return_value = "Repo: SSW567-XuZhou  Number of commits: 7"
         result = getRepo.get_repo_commits('XuZhouCS')
