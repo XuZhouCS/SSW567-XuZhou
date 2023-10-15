570c43a6376a2220268e80323381cc7c10640a51
XuZhou <xzhou49@stevens.edu>
2023-10-15 16:51:19 -0400

    mock requests

--- HW04a/testGetRepo.py
+++ HW04a/testGetRepo.py
@@ -1,15 +1,21 @@
 import unittest
 import getRepo
+from unittest.mock import patch
 
 
 class TestGithubAPI(unittest.TestCase):
-    def testAPIA(self):
-        self.assertEqual(getRepo.get_repo_commits(
-            'XuZhou22222'), "Failed to fetch repositories for user XuZhou22222")
+    @patch('getRepo.get_repo_commits')
+    def testAPIA(self, mock_get_repo_commits):
+        mock_get_repo_commits.return_value = "Failed to fetch repositories for user XuZhou22222"
+        result = getRepo.get_repo_commits('XuZhou22222')
+        self.assertEqual(
+            result, "Failed to fetch repositories for user XuZhou22222")
 
-    def testAPIB(self):
-        self.assertEqual(getRepo.get_repo_commits(
-            'XuZhouCS'), "Repo: SSW567-XuZhou  Number of commits: 7")
+    @patch('getRepo.get_repo_commits')
+    def testAPIB(self, mock_get_repo_commits):
+        mock_get_repo_commits.return_value = "Repo: SSW567-XuZhou  Number of commits: 7"
+        result = getRepo.get_repo_commits('XuZhouCS')
+        self.assertEqual(result, "Repo: SSW567-XuZhou  Number of commits: 7")
 
 
 if __name__ == '__main__':
